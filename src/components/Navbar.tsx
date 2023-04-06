730ac82eb1676b1292f8bfca19bc4681c19acffd
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-06 03:02:08 +0300

    created the main navbar.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import NavContainer from './styles/Nav-styled';
+
+const Nav: React.FC = () => {
+  return (
+    <NavContainer>
+      <h1>
+        <Link className="header-link" to="/">
+          1 % PERFUMES
+        </Link>
+      </h1>
+      <div className="links">
+        <Link to="/products">Products</Link>
+        <Link className="cart-link" to="/cart">
+          <FontAwesomeIcon icon={faCartShopping} />
+          <span>0</span>
+        </Link>
+      </div>
+    </NavContainer>
+  );
+};
+export default Nav;

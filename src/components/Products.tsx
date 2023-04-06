3f4485fb1d57a3f71936848567dbf86280deca3e
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-06 17:41:50 +0300

    Added the products page component

---
+++ src/components/Products.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainNav from './Navbar';
+import SideNav from './SideNav';
+
+const ProductsPage: React.FC = () => {
+  return (
+    <>
+      <MainNav />
+      <SideNav />
+    </>
+  );
+};
+
+export default ProductsPage;

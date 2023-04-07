4dffbfab17d3f90df6ca2c3cd4d38dcd1f9699be
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-07 19:13:59 +0300

    imported the products and loaded them depending on thier path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import HomePage from './components/MainPage';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import MenProducts from './products/MenProducts';
+import WomenProducts from './products/WomenProducts';
+import UnisexProducts from './products/UnisexProducts';
 import ProductsPage from './components/Products';
 
 const App: React.FC = () => {
@@ -8,12 +11,23 @@ const App: React.FC = () => {
     <>
       <HashRouter>
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="Products" element={<ProductsPage />}></Route>
+          <Route path="/" element={<HomePage />} key="home"></Route>
+          <Route
+            path="Products/men"
+            element={<ProductsPage products={MenProducts} key="men" />}
+          ></Route>
+          <Route
+            path="Products/women"
+            element={<ProductsPage products={WomenProducts} key="women" />}
+          ></Route>
+          <Route
+            path="Products/unisex"
+            element={<ProductsPage products={UnisexProducts} key="unisex" />}
+          ></Route>
         </Routes>
       </HashRouter>
     </>
   );
 };
-// configuration
+// Added 3 directories containing the images
 export default App;

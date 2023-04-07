8ac67067b526f9d3e684a9d65b34ee71e8dc5b6c
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-07 22:39:35 +0300

    Added the links array that would change when the route changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import LinkType from './components/styles/shared/LinkType';
 import HomePage from './components/MainPage';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import MenProducts from './products/MenProducts';
 import WomenProducts from './products/WomenProducts';
 import UnisexProducts from './products/UnisexProducts';
 import ProductsPage from './components/Products';
+
+const LinksInfo: LinkType[] = [
+  {
+    name: 'Men',
+    path: '/products/men',
+    active: false,
+  },
+  {
+    name: 'Women',
+    path: '/products/women',
+    active: false,
+  },
+  {
+    name: 'Unisex',
+    path: '/products/unisex',
+    active: false,
+  },
+];
+
+const MenLinks: LinkType[] = [
+  {
+    name: 'Men',
+    path: '/products/men',
+    active: true,
+  },
+  ...LinksInfo.slice(1),
+];
+
+const WomenLinks: LinkType[] = [
+  ...LinksInfo.slice(0, 1),
+  {
+    name: 'Women',
+    path: 'products/women',
+    active: true,
+  },
+  ...LinksInfo.slice(2),
+];
+
+const UnisexLinks: LinkType[] = [
+  ...LinksInfo.slice(0, 2),
+  {
+    name: 'Unisex',
+    path: 'products/unisex',
+    active: true,
+  },
+];
 
 const App: React.FC = () => {
   return (
@@ -13,16 +60,30 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<HomePage />} key="home"></Route>
           <Route
-            path="Products/men"
-            element={<ProductsPage products={MenProducts} key="men" />}
+            path="products/men"
+            element={
+              <ProductsPage products={MenProducts} Links={MenLinks} key="men" />
+            }
           ></Route>
           <Route
-            path="Products/women"
-            element={<ProductsPage products={WomenProducts} key="women" />}
+            path="products/women"
+            element={
+              <ProductsPage
+                products={WomenProducts}
+                Links={WomenLinks}
+                key="women"
+              />
+            }
           ></Route>
           <Route
-            path="Products/unisex"
-            element={<ProductsPage products={UnisexProducts} key="unisex" />}
+            path="products/unisex"
+            element={
+              <ProductsPage
+                products={UnisexProducts}
+                Links={UnisexLinks}
+                key="unisex"
+              />
+            }
           ></Route>
         </Routes>
       </HashRouter>
